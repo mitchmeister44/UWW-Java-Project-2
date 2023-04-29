30b5731051e5ea5100c656788047379c0df6182f
mitchmeister44 <78915565+mitchmeister44@users.noreply.github.com>
2023-04-29 14:44:24 -0500

    Update Duck.java

--- Duck.java
+++ Duck.java
@@ -1,1 +1,32 @@
+public class Duck extends Pet {
+    Duck() {
+        this.health = 3.0;
+        this.attack = 2.0;
+        this.shield = 0.0;
+        this.habitat = "air";
+        this.attackMultiplier = 1.0;
+        this.level = 1;
+    }
 
+    @Override 
+    public void upgrade() {
+        if(level == 1) {
+            this.health = 4.0;
+            this.attack = 4.0;
+        }
+        else if(level == 2) {
+            this.health = 5.0;
+            this.attack = 6.0;
+            this.shield = 1.0;
+        }
+        else {
+            System.out.println("Pet cannot be upgraded, maximum level achieved.");
+        }
+    }
+
+    @Override
+    public void useSpecial(Pet p2) {
+        this.health++;
+        p2.health++;
+    }
+}
