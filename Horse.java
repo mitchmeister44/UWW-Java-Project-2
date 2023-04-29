15eed1c0d026e77551425c7846f36daa9fb21b5b
mitchmeister44 <78915565+mitchmeister44@users.noreply.github.com>
2023-04-29 14:44:58 -0500

    Update Horse.java

--- Horse.java
+++ Horse.java
@@ -1,1 +1,32 @@
+public class Horse extends Pet {
+    Horse() {
+        this.health = 1.0;
+        this.attack = 2.0;
+        this.shield = 2.0;
+        this.habitat = "land";
+        this.attackMultiplier = 1.0;
+        this.level = 1;
+    }
 
+    @Override 
+    public void upgrade() {
+        if(level == 1) {
+            this.health = 2.0;
+            this.attack = 4.0;
+        }
+        else if(level == 2) {
+            this.health = 4.0;
+            this.attack = 5.0;
+            this.shield = 3.0;
+        }
+        else {
+            System.out.println("Pet cannot be upgraded, maximum level achieved.");
+        }
+    }
+
+    @Override
+    public void useSpecial(Pet p2) {
+        this.shield++;
+        p2.shield++;
+    }
+}
