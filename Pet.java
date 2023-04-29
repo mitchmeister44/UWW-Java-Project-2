d73126de094f6b8151e9e31ac3ce671ca23c85a2
mitchmeister44 <78915565+mitchmeister44@users.noreply.github.com>
2023-04-29 14:42:22 -0500

    Update Pet.java

--- Pet.java
+++ Pet.java
@@ -1,4 +1,43 @@
-//Nothing yet, figuring that work here would be redudant thus far with what Hisham has done
-public class Pet {
-    
+public abstract class Pet {
+    public double health;
+    public double attack;
+    public double shield;
+    //Land, air, water, garden
+    //Land trumps water, air trumps land, garden trumps air, water trumps garden
+    public String habitat;
+    public double attackMultiplier;
+    public int level;
+
+    public String getName() {
+        return this.getClass().getSimpleName();
+    }
+
+    public double getAttackMultiplier() {
+        return this.attackMultiplier;
+    }
+
+    public void setAttackMultiplier(Pet p2) {
+        if((this.habitat == "water" && p2.habitat == "land") || (this.habitat == "land" && p2.habitat == "air") || (this.habitat == "air" && p2.habitat == "garden") || (this.habitat == "garden" && p2.habitat == "water")) {
+            attackMultiplier = 0.5;
+        }
+        else if ((this.habitat == "land" && p2.habitat == "water") || (this.habitat == "air" && p2.habitat == "land") || (this.habitat == "garden" && p2.habitat == "air") || (this.habitat == "water" && p2.habitat == "garden")) {
+            attackMultiplier = 2.0;
+        }
+        else if (this.habitat == p2.habitat) {
+            attackMultiplier = 1.0;
+        }
+    }
+
+    public abstract void upgrade();
+    //Garden adds 1 attack point to itself and another pet
+    //Water adds 1 shield point to itself and another pet
+    //Land adds 1 health point to itself and another pet
+    //Air substracts 1 health point from the pet casting its special while doubling the attack of another pet
+    public abstract void useSpecial(Pet p2);
+
+    @Override
+    public String toString() {
+        return String.format("%s:%n %-10s %-10s%n %-10s %-10s%n %-10s %-10s%n %-10s %-10s%n %-10s %-10s%n",getName(), "Tier:", level, "Attack:", attack, "Shield:", shield, "Health:", health, "Habitat:", habitat);
+        //return String.format("%s is of tier %d status, has %.1f attack, %.1f shield, %.1f health, and is of the %s habitat type.",getName(), level, attack, shield, health, habitat);
+    }
 }
