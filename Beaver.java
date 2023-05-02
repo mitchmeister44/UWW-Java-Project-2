0d62a5c3f86cd7f919b7e7f0bdbc1e5cbf82ed5e
mitchmeister44 <78915565+mitchmeister44@users.noreply.github.com>
2023-05-01 21:32:23 -0500

    Update Beaver.java

--- Beaver.java
+++ Beaver.java
@@ -2,10 +2,10 @@ public class Beaver extends Pet {
     Beaver() {
         this.health = 2.0;
         this.attack = 3.0;
-        this.shield = 1.0;
         this.habitat = "water";
         this.attackMultiplier = 1.0;
         this.level = 1;
+        this.luck = 5;
     }
 
     @Override 
@@ -13,11 +13,12 @@ public class Beaver extends Pet {
         if(level == 1) {
             this.health = 3.0;
             this.attack = 4.0;
+            level++;
         }
         else if(level == 2) {
             this.health = 4.0;
             this.attack = 5.0;
-            this.shield = 2.0;
+            level++;
         }
         else {
             System.out.println("Pet cannot be upgraded, maximum level achieved.");
@@ -26,7 +27,7 @@ public class Beaver extends Pet {
 
     @Override
     public void useSpecial(Pet p2) {
-        this.shield++;
-        p2.shield++;
+        this.luck++;
+        p2.luck++;
     }
 }
