a54594ab25d86cfdbdf795aec446fbd4c7007819
mitchmeister44 <78915565+mitchmeister44@users.noreply.github.com>
2023-05-01 21:34:55 -0500

    Update Owl.java

--- Owl.java
+++ Owl.java
@@ -1,1 +1,33 @@
+public class Owl extends Pet {
+    Owl() {
+        this.health = 5.0;
+        this.attack = 3.0;
+        this.habitat = "air";
+        this.attackMultiplier = 1.0;
+        this.level = 1;
+        this.luck = 5;
+    }
 
+    @Override 
+    public void upgrade() {
+        if(level == 1) {
+            this.health = 6.0;
+            this.attack = 5.0;
+            level++;
+        }
+        else if(level == 2) {
+            this.health = 7.0;
+            this.attack = 6.0;
+            level++;
+        }
+        else {
+            System.out.println("Pet cannot be upgraded, maximum level achieved.");
+        }
+    }
+
+    @Override
+    public void useSpecial(Pet p2) {
+        this.health--;
+        p2.health = p2.attack+p2.attack;
+    }
+}
