90dc6e7a0913b943ce0e4141a354490acf658764
mitchmeister44 <78915565+mitchmeister44@users.noreply.github.com>
2023-05-01 21:32:10 -0500

    Update Ant.java

--- Ant.java
+++ Ant.java
@@ -2,21 +2,22 @@ public class Ant extends Pet {
     Ant () {
         this.health = 1.0;
         this.attack = 2.0;
-        this.shield = 0.0;
         this.habitat = "garden";
         this.attackMultiplier = 1.0;
         this.level = 1;
+        this.luck = 5;
     }
     @Override 
     public void upgrade() {
         if(level == 1) {
             this.health = 2.0;
             this.attack = 4.0;
+            level++;
         }
         else if(level == 2) {
             this.health = 3.0;
             this.attack = 6.0;
-            this.shield = 1.0;
+            level++;
         }
         else {
             System.out.println("Pet cannot be upgraded, maximum level achieved.");
