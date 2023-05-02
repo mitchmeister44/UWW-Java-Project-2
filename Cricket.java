b23fdd26d1523ba9d114e9eb770269faa302ba16
mitchmeister44 <78915565+mitchmeister44@users.noreply.github.com>
2023-05-01 21:32:39 -0500

    Update Cricket.java

--- Cricket.java
+++ Cricket.java
@@ -2,10 +2,10 @@ public class Cricket extends Pet {
     Cricket() {
         this.health = 2.0;
         this.attack = 1.0;
-        this.shield = 0.0;
         this.habitat = "garden";
         this.attackMultiplier = 1.0;
         this.level = 1;
+        this.luck = 5;
     }
 
     @Override 
@@ -13,11 +13,12 @@ public class Cricket extends Pet {
         if(level == 1) {
             this.health = 3.0;
             this.attack = 2.0;
+            level++;
         }
         else if(level == 2) {
             this.health = 4.0;
             this.attack = 4.0;
-            this.shield = 1.0;
+            level++;
         }
         else {
             System.out.println("Pet cannot be upgraded, maximum level achieved.");
